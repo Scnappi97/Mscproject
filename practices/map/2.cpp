ae3dab2c63ce5c359798fe941f19d67f3700b771
Schnappi <ymdongzi@126.com>
2020-08-02 23:31:45 +0100

    8.2

--- practices/map/2.cpp
+++ practices/map/2.cpp
@@ -4,13 +4,15 @@
 
 int main(int argc, char **argv)
 {
-  std::map<std::string, int> m;
-  m["hello"] = 23;
+  std::map<std::string, std::map<int,int>> m;
+  std::map<int,int>tmp;
+  tmp[1]=2;
+  m["hello"] = tmp;
   // check if key is present
-  if (m.find("world") != m.end())
+  if (m.find("hello") != m.end())
     std::cout << "map contains key world!\n";
   // retrieve
-  std::cout << m["hello"] << '\n';
+  std::cout << m["hello"][1] << '\n';
   // std::map<std::string, int>::iterator i = m.find("hello");
   // assert(i != m.end());
   // std::cout << "Key: " << i->first << " Value: " << i->second << '\n';
